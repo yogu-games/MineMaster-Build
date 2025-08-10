30cd78fca4d6822440902c72da3602aff26a376a
y0av <mail.yoav@gmail.com>
2025-08-10 15:06:25 +0300

    v0.0.49

--- build/app.js
+++ build/app.js
@@ -15,7 +15,7 @@ var config = {
   streamingAssetsUrl: "StreamingAssets",
   companyName: "YoGu-Games",
   productName: "MineMaster",
-  productVersion: "0.0.47"
+  productVersion: "0.0.49"
 };
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
