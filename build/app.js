caba4172d50544f20538d30499b360c048d55a72
Cloud Build <buildbot@minemaster.xyz>
2025-08-24 08:17:28 +0000

    v0.1.3

--- build/app.js
+++ build/app.js
@@ -7,26 +7,15 @@ var loadingBar = document.querySelector("#unity-loading-bar");
 var progressBarFull = document.querySelector("#unity-progress-bar-full");
 
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/{{{ LOADER_FILENAME }}}";
+var loaderUrl = buildUrl + "/Default WebGL.loader.js";
 var config = {
-  dataUrl: buildUrl + "/{{{ DATA_FILENAME }}}",
-  frameworkUrl: buildUrl + "/{{{ FRAMEWORK_FILENAME }}}",
-#if USE_THREADS
-  workerUrl: buildUrl + "/{{{ WORKER_FILENAME }}}",
-#endif
-#if USE_WASM
-  codeUrl: buildUrl + "/{{{ CODE_FILENAME }}}",
-#endif
-#if MEMORY_FILENAME
-  memoryUrl: buildUrl + "/{{{ MEMORY_FILENAME }}}",
-#endif
-#if SYMBOLS_FILENAME
-  symbolsUrl: buildUrl + "/{{{ SYMBOLS_FILENAME }}}",
-#endif
+  dataUrl: buildUrl + "/Default WebGL.data",
+  frameworkUrl: buildUrl + "/Default WebGL.framework.js",
+  codeUrl: buildUrl + "/Default WebGL.wasm",
   streamingAssetsUrl: "StreamingAssets",
-  companyName: {{{ JSON.stringify(COMPANY_NAME) }}},
-  productName: {{{ JSON.stringify(PRODUCT_NAME) }}},
-  productVersion: {{{ JSON.stringify(PRODUCT_VERSION) }}}
+  companyName: "YoGu-Games",
+  productName: "MineMaster",
+  productVersion: "0.1.3"
 };
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
@@ -39,9 +28,6 @@ if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
   document.getElementsByTagName('head')[0].appendChild(meta);
 }
   
-#if BACKGROUND_FILENAME
-canvas.style.background = "url('" + buildUrl + "/{{{ BACKGROUND_FILENAME.replace(/'/g, '%27') }}}') center / cover";
-#endif
 loadingBar.style.display = "block";
   
 var script = document.createElement("script");
