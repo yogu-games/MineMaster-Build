6e14cbd44b59f384e61c1d2cd21c0303f3248515
Cloud Build <buildbot@minemaster.xyz>
2026-01-27 10:27:38 +0000

    v1.0.149

--- build/app.js
+++ build/app.js
@@ -28,7 +28,7 @@ var config = {
   streamingAssetsUrl: "StreamingAssets",
   companyName: "YoGu-Games",
   productName: "MineMaster",
-  productVersion: "1.0.148"
+  productVersion: "1.0.149"
 };
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
